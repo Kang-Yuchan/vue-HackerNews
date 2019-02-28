9615bd3ca6979eaafd01c0129b17ee74421b5ca3
Kang.Yuchan <ikimaku0517@gmail.com>
2019-02-28 20:51:55 +0900

    add router view

---
+++ src/router/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'news',
+      component: NewsView
+    },
+    {
+      path: '/jobs',
+      name: 'jobs',
+      component: JobsView
+    },
+    {
+      path: '/ask',
+      name: 'ask',
+      component: AskView
+    }
+  ]
+})
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,15 +1,14 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div class="ask">
+        Ask
+    </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
-
 
 <style>
 
